1876195dd6777cb96a17f909d54b97e76efae4f0
karanamtejaswini <karanamtejaswini449@gmail.com>
2017-05-30 15:19:15 +0530

    First assignment

---
+++ src/helloworld.java
@@ -0,0 +1,14 @@
+
+public class helloworld {
+
+	public helloworld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		// TODO Auto-generated method stub
+
+	}
+
+}
